18233f1b0b7ed5f6d59bfdfd88e2b0cea82a103f
Francisco Abalan <f.abalan@raboit.com>
2012-08-22 17:13:29 -0400

    foot

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -85,7 +85,7 @@
 	<?php echo $content; ?>
 
 	
-	<php /* <div id="footer">
+	<?php /* <div id="footer">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
